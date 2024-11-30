15c6a08849a607c1c5944a3789a88d0967a05241
Thuy-Vy Nghiem <thuyvynghiem@yahoo.com>
2024-11-30 20:22:10 +0100

    fix settings change volume n sfx

--- InLovingMemory/Assets/settingsmenu/volumeSettings.cs
+++ InLovingMemory/Assets/settingsmenu/volumeSettings.cs
@@ -14,19 +14,36 @@ public class volumeSettings : MonoBehaviour
 
     private void Start()
     {
-        setMusicVolume();
-        setSfxVolume();
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            loadVolume();
+        }
+        else
+        {
+             setMusicVolume();
+             setSfxVolume();
+        }
     }
 
     public void setMusicVolume()
     {
         float volume = _slider.value;
         _audioMixer.SetFloat("music", Mathf.Log10(volume) * 10);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+    }
+
+    private void loadVolume()
+    {
+        _slider.value = PlayerPrefs.GetFloat("musicVolume");
+        _slider2.value = PlayerPrefs.GetFloat("sfxVolume");
+        setMusicVolume();
+        setMusicVolume();
     }
     
     public void setSfxVolume()
     {
         float sfx = _slider2.value;
         _audioMixer.SetFloat("sfx", Mathf.Log10(sfx) * 10);
+        PlayerPrefs.SetFloat("sfxVolume", sfx);
     }
 }
